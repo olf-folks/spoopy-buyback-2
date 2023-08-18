67ba954fe71fabfb02ca8596b816e795cc6c9829
Firmware <ph1rmw4r3@gmail.com>
2023-08-18 09:49:25 +0000

    modified:   config/settings/base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,13 +1,7 @@
 from pathlib import Path
 from decouple import config, Csv
 
-# settings.py
-
-
-
-
-
-print("Django settings base loaded successfully")
+print("Django base settings loaded successfully")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
